37e5b19c782fe4e67f924672462f9064e467e9c3
mafl20 <mafl20@student.aau.dk>
2025-05-01 00:21:36 +0200

    added text edit, menu bar and status bar

--- main.py
+++ main.py
@@ -1,3 +1,43 @@
 import sys
-import random
-from PySide6 import QtCore as core, QtWidgets as widgets, QtGui as gui
+import PySide6 as ps
+from PySide6 import QtCore as Core, QtWidgets as Widgets, QtGui as GUI
+from PySide6.QtWidgets import QTextEdit as TextEdit, QMenuBar as MenuBar, QStatusBar as StatusBar
+from PySide6.QtGui import QAction as Action
+
+class MainWindow(Widgets.QMainWindow):
+    def __init__(self):
+        super().__init__()
+
+        #--------------------------------------------------------------
+        #> WINDOW
+        self.setWindowTitle("ToolBox")
+        self.setGeometry(100, 100, 800, 600)
+
+        #--------------------------------------------------------------
+        #> MENU BAR
+        self.menu_bar = self.menuBar()
+        file_menu = self.menu_bar.addMenu("File")
+
+        new_action = Action("New", self)
+        new_action.triggered.connect(self.new_file)
+        file_menu.addAction(new_action)
+
+        self.status_bar = StatusBar(self)
+        self.setStatusBar(self.status_bar)
+
+        #--------------------------------------------------------------
+        #> TEXT EDIT FIELD
+        self.text_edit = TextEdit(self)
+        self.text_edit.setGeometry(50, 50, 700, 500)
+
+        self.setCentralWidget(self.text_edit)
+
+    def new_file(self):
+        self.text_edit.clear()
+        self.status_bar.showMessage("New file created", 2000) # 2000ms timeout)
+
+if __name__ == "__main__":
+    app = Widgets.QApplication(sys.argv)
+    main_window = MainWindow()
+    main_window.show()
+    sys.exit(app.exec())
